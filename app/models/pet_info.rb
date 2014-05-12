bc8aa9d34b0cb0ce934833d45cec71ff2e94f89d
shanefitzgerald <shanefitzgerald@Shannons-MacBook-Air.local>
2014-05-12 10:17:53 -0700

    mongoid set up with variables

---
+++ app/models/pet_info.rb
@@ -0,0 +1,22 @@
+class PetInfo
+  include Mongoid::Document
+  field :pet_name, type: String
+  field :pet_age, type: String
+  field :pet_breed, type: String
+  field :chip_number, type: Integer
+  field :tag_number, type: Integer
+  field :vet_name, type: String
+  field :vet_address, type: String
+  field :vet_phone, type: Integer
+  field :key_location, type: String
+  field :food_brand, type: String
+  field :food_location, type: String
+  field :feeding_time, type: Float
+  field :special_feeding_instruction, type: String
+  field :walking_times, type: Float
+  field :walking_route_directions, type: String
+  field :water_instructions, type: String
+  field :special_instructions, type: String
+  field :medicine_location, type: String
+  field :emergency_contact_number, type: Integer
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+petinfos = PetInfo.create([{pet_name: 'Maddie', 
+							pet_age: '10', 
+							pet_breed: 'maltease', 
+							chip_number: '123456', 
+							tag_number: '654321', 
+							vet_name: 'sammy', 
+							vet_address: '123 Vet drive', 
+							vet_phone: '3034567890', 
+							key_location: 'under mat', 
+							food_brand: 'the good stuff', 
+							food_location: 'by the sink', 
+							feeding_time: '5', 
+							special_feeding_instruction: 'none', 
+							walking_times: '6', 
+							walking_route_directions: 'can I get a google api here?', 
+							water_instructions: 'only bottled water', 
+							special_instructions: 'none', 
+							medicine_location: 'by the sink', 
+							emergency_contact_number: '2333456789'}])
