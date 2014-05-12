cacea697f7c79970db547ced0ecb39abbe724f12
shanefitzgerald <shanefitzgerald@Shannons-MacBook-Air.local>
2014-05-12 09:37:47 -0700

    testing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Petsitter::Application.config.secret_key_base = '875579ce772f05e6e3a38f841f3e69965d26eaa467c31e102352d2f60d874b1c4e15fb131053645ed625fdd9ef34a77ec80315068c9a1977896dda9aefe8ec57'
